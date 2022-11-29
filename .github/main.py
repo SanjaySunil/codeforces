e7799f3597f6c86056528e005f6a62d105b63a07
Sanjay Sunil <sanjaysunil@protonmail.com>
2022-11-29 18:16:14 +0000

    fix: added items to gitignore

--- .github/main.py
+++ .github/main.py
@@ -1,4 +1,5 @@
 import os
+# Used `alias` command on mac to run globally
 problem = input('Problem: ')
 os.system('git add .')
 os.system(f'git commit -m "feat({problem[-1]}): solved \`{problem[:-1]}{problem[-1]}\`"')
--- solved_problems/A/71.py
+++
@@ -1,6 +0,0 @@
-test_cases = int(input())
-for i in range(test_cases):
-  inp = str(input())
-
-  if len(inp) > 10: print(f'{inp[0]}{str(len(inp)-2)}{inp[-1]}')
-  else: print(inp)
