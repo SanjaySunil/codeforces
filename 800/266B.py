3e264868dcb9967410b1173c8574edf6e751d0be
Sanjay Sunil <sanjaysunil@protonmail.com>
2022-12-13 11:13:58 +0000

    Create 266B.py

---
+++ 800/266B.py
@@ -0,0 +1,14 @@
+params = list(map(int, input().split()))
+queue = list(input())
+
+for i in range(params[-1]):
+  curr = 0
+  while curr < len(queue) - 1:
+    if queue[curr + 1] == 'G' and queue[curr] == 'B':
+      queue[curr] = 'G'
+      queue[curr+1] = 'B'
+      curr += 2
+    else: curr += 1
+
+print("".join(queue))
+    
